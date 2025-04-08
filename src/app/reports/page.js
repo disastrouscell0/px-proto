86566c2cc158943c9f966bcd6119d3d88556d1c5
Michelo Dipasupil <22121079@student.westernsydney.edu.au>
2025-04-08 22:16:04 +1000

    Reports

--- src/app/reports/page.js
+++ src/app/reports/page.js
@@ -1,5 +1,27 @@
+import Header from '@/components/Header';
+import '@/app/reports/reports.css'
+import Link from 'next/link';
+
 const Reports = () => {
-    return ( <main></main> );
+    return (
+    <>
+    <Header />
+    <main>
+        <div class="content">
+            <div class="cards">
+                <div class="card"><Link href='/reports/semester'>Year 1 2024 1H Report</Link></div>
+                <div class="card"><Link href='/reports/semester'>Year 2 2024 1H Report</Link></div>
+                <div class="card"><Link href='/reports/semester'>Year 3 2024 1H Report</Link></div>
+                <div class="card"><Link href='/reports/semester'>Year 1 2024 2H Report</Link></div>
+                <div class="card"><Link href='/reports/semester'>Year 2 2024 2H Report</Link></div>
+                <div class="card"><Link href='/reports/semester'>Year 3 2024 2H Report</Link></div>
+                <div class="card"><Link href='/reports/semester'>Year 1 2025 1H Report</Link></div>
+                <div class="card"><Link href='/reports/semester'>Year 2 2025 1H Report</Link></div>
+                <div class="card"><Link href='/reports/semester'>Year 3 2025 1H Report</Link></div>
+            </div>
+        </div>
+    </main>
+    </>);
 }
  
 export default Reports;
